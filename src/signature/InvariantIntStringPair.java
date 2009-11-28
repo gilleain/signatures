3fc60d7a55d895722e0f9303eb545430521ac511
maclean <gilleain.torrance@gmail.com>
2009-11-28 21:50:15 +0000

    Sorting string before parent count

--- src/signature/InvariantIntStringPair.java
+++ src/signature/InvariantIntStringPair.java
@@ -21,12 +21,17 @@ public class InvariantIntStringPair implements
     }
 
     public int compareTo(InvariantIntStringPair o) {
-        if (this.value < o.value) {
-            return -1;
-        } else if (this.value > o.value){
-            return 1;
+        int c = this.string.compareTo(o.string);
+        if (c == 0) {
+            if (this.value < o.value) {
+                return -1;
+            } else if (this.value > o.value){
+                return 1;
+            } else {
+                return 0;
+            }
         } else {
-            return this.string.compareTo(o.string);
+            return c;
         }
     }
 
