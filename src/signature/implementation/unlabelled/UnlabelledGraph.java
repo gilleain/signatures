7a09b7b625b829a700ec098eefc9f7c2c0eca768
maclean <gilleain.torrance@gmail.com>
2009-11-29 17:02:50 +0000

    Rearranged the testing framework

---
+++ src/signature/implementation/unlabelled/UnlabelledGraph.java
@@ -0,0 +1,72 @@
+package signature.implementation.unlabelled;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A very simple graph class - the equivalent of a client library class.
+ * 
+ * @author maclean
+ *
+ */
+public class UnlabelledGraph {
+    
+    public class Edge {
+        public int a;
+        public int b;
+        
+        public Edge(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+        
+        public String toString() {
+            return this.a + "-" + this.b;
+        }
+    }
+    
+    public List<Edge> edges;
+    
+    public int maxVertexIndex;
+    
+    public UnlabelledGraph(String graphString) {
+        this.edges = new ArrayList<Edge>();
+        for (String edgeString : graphString.split(",")) {
+            String[] vertexStrings = edgeString.split(":");
+            int a = Integer.parseInt(vertexStrings[0]);
+            int b = Integer.parseInt(vertexStrings[1]);
+            this.edges.add(new Edge(a, b));
+            if (a > maxVertexIndex) maxVertexIndex = a;
+            if (b > maxVertexIndex) maxVertexIndex = b;
+        }
+    }
+    
+    public int getVertexCount() {
+        return this.maxVertexIndex + 1;
+    }
+    
+    public int[] getConnected(int vertexIndex) {
+        List<Integer> connected = new ArrayList<Integer>();
+        for (Edge edge : this.edges) {
+            if (edge.a == vertexIndex) {
+                connected.add(edge.b);
+            } else if (edge.b == vertexIndex) {
+                connected.add(edge.a);
+            } else {
+                continue;
+            }
+        }
+        int[] connectedArray = new int[connected.size()];
+        int i = 0;
+        for (int connectedVertexIndex : connected) {
+            connectedArray[i] = connectedVertexIndex;
+            i++;
+        }
+        return connectedArray;
+    }
+    
+    public String toString() {
+        return edges.toString();
+    }
+    
+}
--- src/test/UnlabelledGraphTest.java
+++ src/test/UnlabelledGraphTest.java
@@ -1,115 +1,26 @@
 package test;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.junit.Test;
 
-import signature.AbstractSignature;
+import signature.implementation.unlabelled.UnlabelledGraph;
+import signature.implementation.unlabelled.UnlabelledGraphSignature;
 
 public class UnlabelledGraphTest {
+    
+    public UnlabelledGraphSignature signatureFromString(String string) {
+        UnlabelledGraph graph = new UnlabelledGraph(string);
+        return new UnlabelledGraphSignature(graph, 0); 
+    }
 	
-	public class Edge {
-		public int a;
-		public int b;
-		
-		public Edge(int a, int b) {
-			this.a = a;
-			this.b = b;
-		}
-		
-		public String toString() {
-		    return this.a + "-" + this.b;
-		}
-	}
+    @Test 
+    public void testChain() {
+        String chain = "0:1,1:2,2:3,3:4";
+        UnlabelledGraphSignature signature = signatureFromString(chain);
+        System.out.println(signature.getDAG());
+        System.out.println(signature.toString());
+        StringBuffer maxSignature = new StringBuffer();
+        signature.canonize(0, maxSignature);
+        System.out.println("max" + maxSignature);
+    }
 	
-	public class UnlabelledGraph {
-		
-		public List<Edge> edges;
-		
-		public int maxVertexIndex;
-		
-		public UnlabelledGraph(String graphString) {
-			this.edges = new ArrayList<Edge>();
-			for (String edgeString : graphString.split(",")) {
-				String[] vertexStrings = edgeString.split(":");
-				int a = Integer.parseInt(vertexStrings[0]);
-				int b = Integer.parseInt(vertexStrings[1]);
-				this.edges.add(new Edge(a, b));
-				if (a > maxVertexIndex) maxVertexIndex = a;
-				if (b > maxVertexIndex) maxVertexIndex = b;
-			}
-		}
-		
-		public int getVertexCount() {
-		    return this.maxVertexIndex + 1;
-		}
-		
-		public int[] getConnected(int vertexIndex) {
-			List<Integer> connected = new ArrayList<Integer>();
-			for (Edge edge : this.edges) {
-				if (edge.a == vertexIndex) {
-					connected.add(edge.b);
-				} else if (edge.b == vertexIndex) {
-					connected.add(edge.a);
-				} else {
-					continue;
-				}
-			}
-			int[] connectedArray = new int[connected.size()];
-			int i = 0;
-			for (int connectedVertexIndex : connected) {
-				connectedArray[i] = connectedVertexIndex;
-				i++;
-			}
-			return connectedArray;
-		}
-		
-		public String toString() {
-		    return edges.toString();
-		}
-		
-	}
-
-	public class UnlabelledGraphSignature extends AbstractSignature {
-		
-		public UnlabelledGraph graph;
-		
-		public UnlabelledGraphSignature(UnlabelledGraph graph, int root) {
-			super();
-			this.graph = graph;
-			super.create(root);
-		}
-
-		public int getVertexCount() {
-		    return this.graph.getVertexCount();
-		}
-		
-		public int[] getConnected(int vertexIndex) {
-			return this.graph.getConnected(vertexIndex);
-		}
-
-		public String getEdgeSymbol(int vertexIndex, int otherVertexIndex) {
-			return "";
-		}
-
-		public String getVertexSymbol(int vertexIndex) {
-			return ".";
-		}
-
-	}
-	
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		String chain = "0:1,0:2,0:3,0:4";
-		UnlabelledGraphTest test = new UnlabelledGraphTest();
-		UnlabelledGraph graph = test.new UnlabelledGraph(chain);
-		UnlabelledGraphSignature signature = 
-			test.new UnlabelledGraphSignature(graph, 0);
-		System.out.println(signature.getDAG());
-		System.out.println(graph + "\n" + signature.toString());
-		StringBuffer maxSignature = new StringBuffer();
-		signature.canonize(0, maxSignature);
-		System.out.println(maxSignature);
-	}
 }
