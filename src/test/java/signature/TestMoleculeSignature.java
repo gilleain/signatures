0199f892eb57f5f4d1675954d373a78159a89880
maclean <gilleain.torrance@gmail.com>
2009-12-13 15:10:09 +0000

    Some minor changes that snuck back in

--- src/test/java/signature/TestMoleculeSignature.java
+++ src/test/java/signature/TestMoleculeSignature.java
@@ -10,34 +10,34 @@ import signature.chemistry.MoleculeSignature;
 
 public class TestMoleculeSignature {
     
-//   @Test
-//    public void minimalTest() {
-//        Molecule molecule = new Molecule();
-//        molecule.addAtom(0, "C");
-//        molecule.addAtom(1, "N");
-//        molecule.addAtom(2, "Cl");
-//        molecule.addAtom(3, "O");
-//        molecule.addBond(0, 1, 1);
-//        molecule.addBond(1, 2, 1);
-//        molecule.addBond(1, 3, 1);
-//        MoleculeSignature signature = new MoleculeSignature(molecule);
-//        String moleculeSignature = signature.getGraphSignature();
-//        //System.out.println("molsig : " + moleculeSignature);
-//        
-//        Molecule permutation = new Molecule();
-//        permutation.addAtom(0, "C");
-//        permutation.addAtom(1, "N");
-//        permutation.addAtom(2, "O");
-//        permutation.addAtom(3, "Cl");
-//        permutation.addBond(0, 1, 1);
-//        permutation.addBond(1, 2, 1);
-//        permutation.addBond(1, 3, 1);
-//        MoleculeSignature permSignature = new MoleculeSignature(molecule);
-//        String permSignatureString = permSignature.getGraphSignature();
-//        //System.out.println("molsig : " + permSignatureString);
-//        
-//        Assert.assertEquals(moleculeSignature, permSignatureString);
-//    }
+   @Test
+    public void minimalTest() {
+        Molecule molecule = new Molecule();
+        molecule.addAtom(0, "C");
+        molecule.addAtom(1, "N");
+        molecule.addAtom(2, "Cl");
+        molecule.addAtom(3, "O");
+        molecule.addBond(0, 1, 1);
+        molecule.addBond(1, 2, 1);
+        molecule.addBond(1, 3, 1);
+        MoleculeSignature signature = new MoleculeSignature(molecule);
+        String moleculeSignature = signature.getGraphSignature();
+        //System.out.println("molsig : " + moleculeSignature);
+        
+        Molecule permutation = new Molecule();
+        permutation.addAtom(0, "C");
+        permutation.addAtom(1, "N");
+        permutation.addAtom(2, "O");
+        permutation.addAtom(3, "Cl");
+        permutation.addBond(0, 1, 1);
+        permutation.addBond(1, 2, 1);
+        permutation.addBond(1, 3, 1);
+        MoleculeSignature permSignature = new MoleculeSignature(molecule);
+        String permSignatureString = permSignature.getGraphSignature();
+        //System.out.println("molsig : " + permSignatureString);
+        
+        Assert.assertEquals(moleculeSignature, permSignatureString);
+    }
     
     @Test
     public void testLargeExample() {
@@ -58,7 +58,7 @@ public class TestMoleculeSignature {
     	//String filename = "/home/lc/Molecules/atomPermuterForOB4.sdf";
         int molNr = 0;
         for (Molecule molecule : MoleculeReader.readSDFFile(filename)) {
-        	//System.out.println(++molNr);
+        	System.out.println(++molNr);
             MoleculeSignature signature = new MoleculeSignature(molecule);
             System.out.println(signature.getGraphSignature());
             //System.out.println(signature.getVertexSignature(0));
