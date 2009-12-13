5759eb1ea0fef1067c93c01340d96201c3517cda
Lars Carlsson <larscarlsson@yahoo.com>
2009-12-13 23:15:59 +0100

    Added som non-working code for canonical labelling.

--- src/test/TestMoleculeSignature.java
+++ src/test/TestMoleculeSignature.java
@@ -1,5 +1,8 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import junit.framework.Assert;
 
 import org.junit.Test;
@@ -52,18 +55,69 @@ public class TestMoleculeSignature {
     public void testSDF() {
         //String filename = "data/test.sdf";
         //String filename = "/home/lc/Molecules/ChEBI_complete.sdf";
-        //String filename = "/home/lc/disconnected.sdf";
+        //String filename = "/home/lc/Molecules/fourAtoms.sdf";
+        //String filename = "/home/lc/Molecules/fourAtomsCanLabel.sdf";
     	//String filename = "/home/lc/Molecules/atomPermuter.sdf";
-    	String filename = "/home/lc/Molecules/atomPermuterForOB0.sdf";
+    	//String filename = "/home/lc/Molecules/atomPermuterForOB0.sdf";
+    	//String filename = "/home/lc/TestSignatures/atomPermuterForOB0Perm.sdf";
+    	//String filename = "/home/lc/Molecules/atomPermuterForOB0CanLabel.sdf";
     	//String filename = "/home/lc/Molecules/atomPermuterForOB4.sdf";
-        int molNr = 0;
+    	//String filename = "/home/lc/TestSignatures/unlabeled.sdf";
+    	//String filename = "/home/lc/TestSignatures/unlabeledCanLabel.sdf";
+    	//String filename = "/home/lc/TestSignatures/simple.sdf";
+    	//String filename = "/home/lc/TestSignatures/simplePerm.sdf";
+    	//String filename = "/home/lc/TestSignatures/multCycle.sdf";
+    	//String filename = "/home/lc/TestSignatures/multCyclePerm.sdf";
+    	String filename = "/home/lc/TestSignatures/cyclicButane.sdf";
+    	int molNr = 0;
         for (Molecule molecule : MoleculeReader.readSDFFile(filename)) {
         	//System.out.println(++molNr);
             MoleculeSignature signature = new MoleculeSignature(molecule);
+            signature.canonicalLabelMapping = new ArrayList<List<Integer>>();
+
             System.out.println(signature.getGraphSignature());
             //System.out.println(signature.getVertexSignature(0));
+            System.out.println(signature.canonicalLabelMapping.toString());
         }
             
+    }
+    
+    @Test
+    public void testHeightVertexSignature() {
+    	// This generates a height two vertex signature of a graph defined by a single modlecule in an SD-file.
+    	String filename = "/home/lc/Molecules/atomPermuterForOB0.sdf";
+    	
+    	List<String> vertexSignatures;
+    	
+    	
+    }
+
+    
+    @Test
+    public void unlabelledTest() {
+        Molecule molecule = new Molecule();
+        molecule.addAtom(0, "C");
+        molecule.addAtom(1, "C");
+        molecule.addAtom(2, "C");
+        molecule.addBond(0, 2, 1);
+        molecule.addBond(1, 2, 1);
+        MoleculeSignature signature = new MoleculeSignature(molecule);
+        signature.canonicalLabelMapping = new ArrayList<List<Integer>>();
+        String moleculeSignature = signature.getGraphSignature();
+        System.out.println("molsig : " + moleculeSignature);
+        
+        Molecule permutation = new Molecule();
+        permutation.addAtom(0, "C");
+        permutation.addAtom(1, "C");
+        permutation.addAtom(2, "C");
+        permutation.addBond(0, 1, 1);
+        permutation.addBond(1, 2, 1);
+        MoleculeSignature permSignature = new MoleculeSignature(molecule);
+        permSignature.canonicalLabelMapping = new ArrayList<List<Integer>>();
+        String permSignatureString = permSignature.getGraphSignature();
+        System.out.println("permMolsig : " + permSignatureString);
+        
+        Assert.assertEquals(moleculeSignature, permSignatureString);
     }
 
 }
