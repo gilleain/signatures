4e75103101c5fc0d231fa445a7dd0e6f1eeaf6be
maclean <gilleain.torrance@gmail.com>
2009-12-15 20:44:21 +0000

    Some cleanup to the canonical code

--- src/main/java/signature/AbstractSignature.java
+++ src/main/java/signature/AbstractSignature.java
@@ -8,7 +8,7 @@ import java.util.List;
  * The abstract base class for signatures. This class creates and manages a DAG
  * and the input/output of signature strings.
  * 
- * To properly subclass this class, the constuctor of the derived class should
+ * To properly subclass this class, the constructor of the derived class should
  * follow this pattern:
  * <pre>
  *   public MySignature(GRAPH_TYPE inputGraph, int rootVertexIndex) {
@@ -39,9 +39,7 @@ public abstract class AbstractSignature {
 	
 	private DAG dag;
 	
-	private boolean isCanonicalLabel = false;
-	
-	public List<List<Integer>> canonicalLabelMapping;
+	private List<List<Integer>> canonicalLabelMapping;
 
 	private List<Integer> currentCanonicalLabelMapping;
 	
@@ -49,16 +47,14 @@ public abstract class AbstractSignature {
 	
 	private String graphSignature;
 
-	private ArrayList<String> unsortedCopyVertexSignatures;
-
 	public AbstractSignature() {
-		this.startNodeSymbol = "";
-		this.endNodeSymbol = "";
+		this("", "");
 	}
 	
 	public AbstractSignature(String startNodeSymbol, String endNodeSymbol) {
-		this.startNodeSymbol = startNodeSymbol;
-		this.endNodeSymbol = endNodeSymbol;
+	    this.startNodeSymbol = startNodeSymbol;
+	    this.endNodeSymbol = endNodeSymbol;
+	    this.canonicalLabelMapping = new ArrayList<List<Integer>>();
 	}
 	
 	public DAG getDAG() {
@@ -76,11 +72,6 @@ public abstract class AbstractSignature {
         // assume that the atom invariants have been initialized
         if (this.getVertexCount() == 0) return;
         
-        // Check if the canonicalLabelMapping is intialized.
-        if ( this.canonicalLabelMapping == null ) {
-        	this.canonicalLabelMapping = new ArrayList<List<Integer>>();
-        }
-        
         this.dag.updateVertexInvariants();
         
         List<Integer> orbit = this.dag.createOrbit();
@@ -92,14 +83,14 @@ public abstract class AbstractSignature {
                 this.dag.setColor(pair.index, color);
                 color++;
             }
-            //this.currentCanonicalLabelMapping = new ArrayList<Integer>();
-            String signature = this.toString(); // Creating the root signature string.
+            // Creating the root signature string.
+            String signature = this.toString(); 
             if (signature.compareTo(canonicalVertexSignature.toString()) > 0) {
                 int l = canonicalVertexSignature.length();
                 canonicalVertexSignature.replace(0, l, signature);
-                //this.canonicalLabelMapping = this.currentCanonicalLabelMapping;
-                copyToCanonicalLabelMapping(this.currentCanonicalLabelMapping);
-                //System.out.println(signature + this.currentCanonicalLabelMapping.toString());
+                this.canonicalLabelMapping.set(
+                        this.canonicalLabelMapping.size() - 1,
+                        this.currentCanonicalLabelMapping);
             }
             return;
         } else {
@@ -113,62 +104,61 @@ public abstract class AbstractSignature {
         }
     }
 	
-	public void create(int rootVertexIndex) {
-	    if (this.getVertexCount() == 0) return;
-		this.dag = new DAG(rootVertexIndex, 
-		                   this.getVertexCount(), 
-		                   this.getVertexSymbol(rootVertexIndex));
-		build(1, this.dag.getRootLayer(), new ArrayList<DAG.Arc>());
-		this.dag.initialize();
-	}
-	
-	private void build(int layer, 
-	        List<DAG.Node> previousLayer, List<DAG.Arc> usedArcs) {
-		List<DAG.Node> nextLayer = new ArrayList<DAG.Node>();
-		List<DAG.Arc> layerArcs = new ArrayList<DAG.Arc>();
-		for (DAG.Node node : previousLayer) {
-			for (int connectedVertex : getConnected(node.vertexIndex)) {
-				addNode(
-				  layer, node, connectedVertex, layerArcs, usedArcs, nextLayer);
-			}
-		}
-		usedArcs.addAll(layerArcs);
-		if (nextLayer.isEmpty()) {
-			return;
-		} else {
-			dag.addLayer(nextLayer);
-			build(layer + 1, nextLayer, usedArcs);
-		}
+    public void create(int rootVertexIndex) {
+        if (this.getVertexCount() == 0) return;
+        this.dag = new DAG(rootVertexIndex, 
+                this.getVertexCount(), 
+                this.getVertexSymbol(rootVertexIndex));
+        build(1, this.dag.getRootLayer(), new ArrayList<DAG.Arc>());
+        this.dag.initialize();
+    }
+
+	private void build(
+	        int layer, List<DAG.Node> previousLayer, List<DAG.Arc> usedArcs) {
+	    List<DAG.Node> nextLayer = new ArrayList<DAG.Node>();
+	    List<DAG.Arc> layerArcs = new ArrayList<DAG.Arc>();
+	    for (DAG.Node node : previousLayer) {
+	        for (int vertex : getConnected(node.vertexIndex)) {
+	            addNode(layer, node, vertex, layerArcs, usedArcs, nextLayer);
+	        }
+	    }
+	    usedArcs.addAll(layerArcs);
+	    if (nextLayer.isEmpty()) {
+	        return;
+	    } else {
+	        dag.addLayer(nextLayer);
+	        build(layer + 1, nextLayer, usedArcs);
+	    }
 	}
 
 	private void addNode(int layer, DAG.Node parentNode, int vertexIndex,
-			List<DAG.Arc> layerArcs, List<DAG.Arc> usedArcs, 
-			List<DAG.Node> nextLayer) {
-		DAG.Arc arc = dag.new Arc(parentNode.vertexIndex, vertexIndex);
-		if (usedArcs.contains(arc)) return;
-		DAG.Node existingNode = null;
-		for (DAG.Node otherNode : nextLayer) {
-			if (otherNode.vertexIndex == vertexIndex) {
-				existingNode = otherNode;
-				break;
-			}
-		}
-		if (existingNode == null) {
-			existingNode = dag.makeNode(
-			        vertexIndex, layer, getVertexSymbol(vertexIndex));
-			nextLayer.add(existingNode);
-		}
-		dag.addRelation(existingNode, parentNode);
-		layerArcs.add(arc);
+	        List<DAG.Arc> layerArcs, List<DAG.Arc> usedArcs, 
+	        List<DAG.Node> nextLayer) {
+	    DAG.Arc arc = dag.new Arc(parentNode.vertexIndex, vertexIndex);
+	    if (usedArcs.contains(arc)) return;
+	    DAG.Node existingNode = null;
+	    for (DAG.Node otherNode : nextLayer) {
+	        if (otherNode.vertexIndex == vertexIndex) {
+	            existingNode = otherNode;
+	            break;
+	        }
+	    }
+	    if (existingNode == null) {
+	        existingNode = dag.makeNode(
+	                vertexIndex, layer, getVertexSymbol(vertexIndex));
+	        nextLayer.add(existingNode);
+	    }
+	    dag.addRelation(existingNode, parentNode);
+	    layerArcs.add(arc);
 	}
-	
+
 	/**
 	 * Get the number of vertices.
 	 * 
 	 * @return the number of vertices in the  input graph
 	 */
 	public abstract int getVertexCount();
-	
+
 	/**
 	 * Get the symbol to use in the output signature string for this vertex of 
 	 * the input graph.
@@ -198,11 +188,13 @@ public abstract class AbstractSignature {
 	public abstract String getEdgeSymbol(int vertexIndex, int otherVertexIndex);
 	
 	public String toString() {
-		StringBuffer buffer = new StringBuffer();
-		print(buffer, this.dag.getRoot(), null, new ArrayList<DAG.Arc>());
-		return buffer.toString();
+	    StringBuffer buffer = new StringBuffer();
+	    this.currentCanonicalLabelMapping = new ArrayList<Integer>();
+	    this.canonicalLabelMapping.add(this.currentCanonicalLabelMapping);
+	    print(buffer, this.dag.getRoot(), null, new ArrayList<DAG.Arc>());
+	    return buffer.toString();
 	}
-	
+
 	/**
 	 * Recursively print the signature into the buffer.
 	 * 
@@ -212,112 +204,102 @@ public abstract class AbstractSignature {
 	 * @param arcs the list of already visited arcs
 	 */
 	private void print(StringBuffer buffer, DAG.Node node,
-			DAG.Node parent, List<DAG.Arc> arcs) {
-		
-		// print out any symbol for the edge in the input graph
-		if (parent != null) {
-			buffer.append(getEdgeSymbol(node.vertexIndex, parent.vertexIndex));
-		}
-		
-		// print out the text that represents the node itself
-		buffer.append(this.startNodeSymbol);
-		buffer.append(getVertexSymbol(node.vertexIndex));
-		// Add the vertexIndex if it hasn't already been added.
-		// First check if it has been created. is this a good way of doing it?
-		if ( this.currentCanonicalLabelMapping == null ) {
-			this.currentCanonicalLabelMapping = new ArrayList<Integer>();
-		}
-		if ( !(inCurrentCanonicalMapping(node.vertexIndex)) ){
-			this.currentCanonicalLabelMapping.add(node.vertexIndex);
-		}
-		int color = dag.colorFor(node.vertexIndex);
-		if (color != 0) {
-			buffer.append(',').append(color);
-		}
-		buffer.append(this.endNodeSymbol);
-		
-		// Need to sort the children here, so that they are printed in an order 
-		// according to their invariants.
-		Collections.sort(node.children);
-		
-		// now print the sorted children, surrounded by branch symbols
-		boolean addedBranchSymbol = false;
-		for (DAG.Node child : node.children) {
-			DAG.Arc arc = dag.new Arc(node.vertexIndex, child.vertexIndex);
-			if (arcs.contains(arc)) {
-				continue;
-			} else {
-				if (!addedBranchSymbol) {
-					buffer.append(AbstractSignature.START_BRANCH_SYMBOL);
-					addedBranchSymbol = true;
-				}
-				arcs.add(arc);
-				print(buffer, child, node, arcs);
-			}
-		}
-		if (addedBranchSymbol) {
-			buffer.append(AbstractSignature.END_BRANCH_SYMBOL);
-		}
+	        DAG.Node parent, List<DAG.Arc> arcs) {
+
+	    // Add the vertexIndex to the labels if it hasn't already been added.
+	    if (!(this.currentCanonicalLabelMapping.contains(node.vertexIndex))) {
+	        this.currentCanonicalLabelMapping.add(node.vertexIndex);
+	    }
+
+	    // print out any symbol for the edge in the input graph
+	    if (parent != null) {
+	        buffer.append(getEdgeSymbol(node.vertexIndex, parent.vertexIndex));
+	    }
+
+	    // print out the text that represents the node itself
+	    buffer.append(this.startNodeSymbol);
+	    buffer.append(getVertexSymbol(node.vertexIndex));
+
+	    int color = dag.colorFor(node.vertexIndex);
+	    if (color != 0) {
+	        buffer.append(',').append(color);
+	    }
+	    buffer.append(this.endNodeSymbol);
+
+	    // Need to sort the children here, so that they are printed in an order 
+	    // according to their invariants.
+	    Collections.sort(node.children);
+
+	    // now print the sorted children, surrounded by branch symbols
+	    boolean addedBranchSymbol = false;
+	    for (DAG.Node child : node.children) {
+	        DAG.Arc arc = dag.new Arc(node.vertexIndex, child.vertexIndex);
+	        if (arcs.contains(arc)) {
+	            continue;
+	        } else {
+	            if (!addedBranchSymbol) {
+	                buffer.append(AbstractSignature.START_BRANCH_SYMBOL);
+	                addedBranchSymbol = true;
+	            }
+	            arcs.add(arc);
+	            print(buffer, child, node, arcs);
+	        }
+	    }
+	    if (addedBranchSymbol) {
+	        buffer.append(AbstractSignature.END_BRANCH_SYMBOL);
+	    }
 	}
-	
+
 	private void resetDAG(int vertexRootIndex) {
 	    String rootSymbol = this.getVertexSymbol(vertexRootIndex);
 	    dag.resetDAG(vertexRootIndex, rootSymbol);
 	    build(1, this.dag.getRootLayer(), new ArrayList<DAG.Arc>());
-        this.dag.initialize();
+	    this.dag.initialize();
 	}
 	
 	private void generateVertexSignatures() {
-		// Loop through all vertices and create a vertex signature for each one them.
-
-		this.create(0);
-	    this.canonicalLabelMapping.add(new ArrayList<Integer>());
-		this.vertexSignatures = new ArrayList<String>();
-		this.vertexSignatures.add(this.toCanonicalVertexString());
-        for (int vertexIndex = 1; vertexIndex < this.getVertexCount(); vertexIndex++) {
-		    this.resetDAG(vertexIndex);
-		    this.canonicalLabelMapping.add(new ArrayList<Integer>());
-			this.vertexSignatures.add(this.toCanonicalVertexString());
-		}
+	    // Loop through all vertices and create a vertex signature for each
+	    this.create(0);
+	    this.vertexSignatures = new ArrayList<String>();
+	    this.vertexSignatures.add(this.toCanonicalVertexString());
+	    int count = this.getVertexCount();
+	    for (int vertexIndex = 1; vertexIndex < count; vertexIndex++) {
+	        this.resetDAG(vertexIndex);
+	        this.vertexSignatures.add(this.toCanonicalVertexString());
+	    }
 	}
-	
 	
 	/**
 	 * Use the lexicographically largest (or smallest) as the graph signature
 	 */
 	private void generateGraphSignature() {
-		this.canonicalLabelMapping = new ArrayList<List<Integer>>();
-		this.generateVertexSignatures();
-		Collections.sort(this.vertexSignatures);
-		this.graphSignature = this.vertexSignatures.get(0);
+	    this.generateVertexSignatures();
+	    Collections.sort(this.vertexSignatures);
+	    this.graphSignature = this.vertexSignatures.get(0);
 	}
-	
-	
+
 	/**
 	 * Convenience function that creates a String to hold the 
-     * signature instead of a StringBuffer.
-     * 
+	 * signature instead of a StringBuffer.
+	 * 
 	 * @return
 	 */
 	private String toCanonicalVertexString() {
-		StringBuffer canonicalVertexStringBuffer = new StringBuffer();
-//		this.canonize(0, canonicalVertexStringBuffer);
-		this.canonize(1, canonicalVertexStringBuffer);
-		return canonicalVertexStringBuffer.toString();
+	    StringBuffer canonicalVertexStringBuffer = new StringBuffer();
+	    this.canonize(1, canonicalVertexStringBuffer);
+	    return canonicalVertexStringBuffer.toString();
 	}
-	
 
 	public String getGraphSignature(){
-		// Generates and returns a graph signature
-		this.generateGraphSignature();
-		return this.graphSignature;
+	    // Generates and returns a graph signature
+	    this.generateGraphSignature();
+	    return this.graphSignature;
 	}
 
-	
 	public String getVertexSignature(int vertexId){
-		// Generates and returns a graph signature
-		this.generateVertexSignatures();
-		return this.vertexSignatures.get(vertexId);
+	    // Generates and returns a graph signature
+	    this.generateVertexSignatures();
+	    return this.vertexSignatures.get(vertexId);
 	}
 
     public List<String> getAllVertexSignatures() {
@@ -326,88 +308,65 @@ public abstract class AbstractSignature {
     }
     
     public boolean isCanonicallyLabelled() {
-    	// Initialize the canonicalLabelMapping here. I'm not sure where this should be initialized.
-		this.canonicalLabelMapping = new ArrayList<List<Integer>>();
-    	// Generate the vertex signatures and identify the graph signature.
-    	this.generateVertexSignatures();
-    	
-    	// Deep copy the vertex signatures so that we know the original order. Move this to a function?
-		this.unsortedCopyVertexSignatures = new ArrayList<String>();
-		for (String vertexString : this.vertexSignatures) {
-			String tmpString = vertexString;			
-			unsortedCopyVertexSignatures.add(new String(tmpString.getBytes()));
-		}
-		Collections.sort(this.vertexSignatures);
-		this.graphSignature = this.vertexSignatures.get(0);
-		
-		// See which of the vertex signatures match the graph signature and return true if any of these are ordered 0, 1, ..., n-1.
-		// Where n is the number of vertices in the graph.
-		int el = 0;
-		for (String vertexSignature : unsortedCopyVertexSignatures) {
-			if ( this.graphSignature.equals(vertexSignature) ) {
-				//System.out.println(this.graphSignature);
-				if ( isInIncreasingOrder(this.canonicalLabelMapping.get(el)) ) {
-					return true;
-				}
-			}
-		el++;
-		}
-		return false;
+        // Generate the vertex signatures and identify the graph signature.
+        this.generateVertexSignatures();
+
+        // Sort a copy of the vertex signatures thus keeping the original order. 
+        List<String> sortedVertexSignatures = new ArrayList<String>();
+        sortedVertexSignatures.addAll(this.vertexSignatures);
+        Collections.sort(sortedVertexSignatures);
+
+        this.graphSignature = sortedVertexSignatures.get(0);
+
+        // See which of the vertex signatures match the graph signature and 
+        // return true if any of these are ordered 0, 1, ..., n-1.
+        // Where n is the number of vertices in the graph.
+        int el = 0;
+        for (String vertexSignature : sortedVertexSignatures) {
+            List<Integer> current = this.canonicalLabelMapping.get(el); 
+            if (this.graphSignature.equals(vertexSignature)
+                    && isInIncreasingOrder(current)) {
+                return true;
+            }
+            el++;
+        }
+        return false;
     }
     
     public List<Integer> canonicalLabel() {
-    	List<Integer> mapping = new ArrayList<Integer>();
-    	
-    	int elementToChange = 0;
-    	while (!this.isCanonicallyLabelled()) {
-    		// Reorder the vertices of the graph.
-    		System.out.println(this.graphSignature);
-    		// Look for the vertexSignature corresponding to the graphSignature that has the lowest vertexId in the elementToChange position.
-    		int el = 0; int minValue = this.getVertexCount();
-    		for (String vertexSignature : unsortedCopyVertexSignatures) {
-    			if ( this.graphSignature.equals(vertexSignature) ) {
-    				//System.out.println(this.graphSignature);
-    				if ( minValue > this.canonicalLabelMapping.get(el).get(elementToChange) ){
-    					minValue = this.canonicalLabelMapping.get(el).get(elementToChange);
-    				}
-    			}
-    		}
-    		// Swap the order of vertexId minValue and elementToChange.
-    		// do the swapping here.
-    		mapping.add(minValue);
-    		elementToChange++;
-    	}
-    	
-    	return mapping;
+        List<Integer> mapping = new ArrayList<Integer>();
+
+        int elementToChange = 0;
+        while (!this.isCanonicallyLabelled()) {
+            // Reorder the vertices of the graph.
+            // Look for the vertexSignature corresponding to the graphSignature 
+            // that has the lowest vertexId in the elementToChange position.
+            int el = 0; 
+            int minValue = this.getVertexCount();
+            List<Integer> currentLabels =  this.canonicalLabelMapping.get(el);
+            for (String vertexSignature : this.vertexSignatures) {
+                if ( this.graphSignature.equals(vertexSignature) ) {
+                    int i = currentLabels.get(elementToChange);
+                    if (minValue > i) {
+                        minValue = i;
+                    }
+                }
+            }
+            // Swap the order of vertexId minValue and elementToChange.
+            // do the swapping here.
+            mapping.add(minValue);
+            elementToChange++;
+        }
+
+        return mapping;
     }
     
     private boolean isInIncreasingOrder(List<Integer> integerList) {
-    	for (int i = 1; i < integerList.size(); i++) {
-    		if ( integerList.get(i-1) > integerList.get(i) ) {
-    			return false;
-    		}
-    	}
-    	return true;
-    }
-    private boolean inCurrentCanonicalMapping(int id) {
-    	if ( this.currentCanonicalLabelMapping.isEmpty() ) {
-    		return false;
-    	}
-    	for (int el : this.currentCanonicalLabelMapping){
-    		if ( el == id )
-    			return true;
-    	}
-		return false;
-    	
-    }
-    
-    private void copyToCanonicalLabelMapping(List<Integer> labelList) {
-    	if (this.canonicalLabelMapping.isEmpty()) {
-    		this.canonicalLabelMapping.add(new ArrayList<Integer>());
-    	}
-    	this.canonicalLabelMapping.get(this.canonicalLabelMapping.size()-1).clear();
-    	for ( int l : labelList ) {
-    		this.canonicalLabelMapping.get(this.canonicalLabelMapping.size()-1).add(l);
-    	}
+        for (int i = 1; i < integerList.size(); i++) {
+            if (integerList.get(i - 1) > integerList.get(i)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
