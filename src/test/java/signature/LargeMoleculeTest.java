7f8d32fcd82163e689f193271c7677914712a8d7
maclean <gilleain.torrance@gmail.com>
2010-04-12 15:36:43 +0100

    Smaller test case for multiple rings

--- src/test/java/signature/LargeMoleculeTest.java
+++ src/test/java/signature/LargeMoleculeTest.java
@@ -7,14 +7,25 @@ import signature.chemistry.MoleculeSignature;
 
 public class LargeMoleculeTest {
     
-    public void addPhenyl(int atomToAttachTo, Molecule molecule) {
+    public void addRing(int atomToAttachTo, int ringSize, Molecule molecule) {
         int numberOfAtoms = molecule.getAtomCount();
         int previous = atomToAttachTo;
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < ringSize; i++) {
             molecule.addAtom("C");
-            molecule.addSingleBond(previous, numberOfAtoms + i);
+            int current = numberOfAtoms + i;
+            molecule.addSingleBond(previous, current);
+            previous = current;
         }
-        molecule.addSingleBond(numberOfAtoms, numberOfAtoms + 5);
+        molecule.addSingleBond(numberOfAtoms, numberOfAtoms + (ringSize - 1));
+    }
+    
+    public Molecule makeMinimalMultiRing(int ringCount, int ringSize) {
+        Molecule mol = new Molecule();
+        mol.addAtom("C");
+        for (int i = 0; i < ringCount; i++) {
+            addRing(0, ringSize, mol);
+        }
+        return mol;
     }
     
     public Molecule makeTetrakisTriphenylPhosphoranylRhodium() {
@@ -27,7 +38,7 @@ public class LargeMoleculeTest {
         
         for (int j = 1; j < 5; j++) {
             for (int k = 0; k < 3; k++) {
-                addPhenyl(j, ttpr);
+                addRing(j, 6, ttpr);
             }
         }
         return ttpr;
@@ -39,6 +50,15 @@ public class LargeMoleculeTest {
         MoleculeSignature molSig = new MoleculeSignature(ttpr);
         String sigString = molSig.toCanonicalString();
         System.out.println(sigString);
+    }
+    
+    @Test
+    public void testMinimalMol() {
+        Molecule mol = makeMinimalMultiRing(6, 3);
+        MoleculeSignature molSig = new MoleculeSignature(mol);
+        String sigString = molSig.toCanonicalString();
+        System.out.println(mol);
+        System.out.println("result " + sigString);
     }
     
     public Molecule makeChain(int length) {
@@ -56,11 +76,16 @@ public class LargeMoleculeTest {
     
     @Test
     public void testLongChains() {
-        int length = 100;
+        int length = 10;
         Molecule chain = makeChain(length);
         MoleculeSignature molSig = new MoleculeSignature(chain);
         String sigString = molSig.toCanonicalString();
         System.out.println(sigString);
+    }
+    
+    public static void main(String[] args) {
+//        new LargeMoleculeTest().testMinimalMol();
+        new LargeMoleculeTest().ttprTest();
     }
 
 }
