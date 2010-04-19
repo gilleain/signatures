f184b9aac50ce5770f8608c7201cb357097f7d5d
maclean <gilleain.torrance@gmail.com>
2010-04-19 13:48:58 +0100

    Beneze multiple bond test

--- src/test/java/signature/chemistry/MultipleBondTest.java
+++ src/test/java/signature/chemistry/MultipleBondTest.java
@@ -1,11 +1,28 @@
 package signature.chemistry;
 
+import junit.framework.Assert;
+
 import org.junit.Test;
 
 public class MultipleBondTest {
+
+    @Test
+    public void cyclobut_1_ene() {
+        Molecule mol = new Molecule();
+        for (int i = 0; i < 4; i++) {
+            mol.addAtom("C");
+        }
+        mol.addBond(0, 1, 1);
+        mol.addBond(0, 2, 2);
+        mol.addBond(1, 3, 1);
+        mol.addBond(2, 3, 1);
+        MoleculeSignature molSignature = new MoleculeSignature(mol);
+        System.out.println(molSignature.toCanonicalString());
+    }
+
     
     @Test
-    public void cyclobutene() {
+    public void cyclobut_2_ene() {
         Molecule mol = new Molecule();
         for (int i = 0; i < 4; i++) {
             mol.addAtom("C");
@@ -16,6 +33,26 @@ public class MultipleBondTest {
         mol.addBond(2, 3, 1);
         MoleculeSignature molSignature = new MoleculeSignature(mol);
         System.out.println(molSignature.toCanonicalString());
+    }
+    
+    @Test
+    public void benzeneTest() {
+        Molecule mol = new Molecule();
+        for (int i = 0; i < 6; i++) {
+            mol.addAtom("C");
+        }
+        mol.addBond(0, 1, 1);
+        mol.addBond(1, 2, 2);
+        mol.addBond(2, 3, 1);
+        mol.addBond(3, 4, 2);
+        mol.addBond(4, 5, 1);
+        mol.addBond(5, 0, 2);
+        MoleculeSignature molSig = new MoleculeSignature(mol);
+        String sigString0 = molSig.signatureStringForVertex(0);
+        for (int i = 1; i < 6; i++) {
+            String sigStringI = molSig.signatureStringForVertex(i);
+            Assert.assertEquals(sigString0, sigStringI);
+        }
     }
 
 }
