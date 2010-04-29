072c53a3fe58986f471f7e177fbf98c7a4886779
maclean <gilleain.torrance@gmail.com>
2010-04-29 12:25:55 +0100

    Added a new permutation test

--- src/test/java/signature/PermutationTest.java
+++ src/test/java/signature/PermutationTest.java
@@ -12,18 +12,11 @@ import signature.chemistry.MoleculeSignature;
 
 public class PermutationTest {
     
-    @Test
-    public void permuteMolecule() {
-        Molecule molecule = new Molecule();
-        molecule.addAtom("C");
-        molecule.addAtom("N");
-        molecule.addAtom("O");
-        molecule.addBond(0, 1, 1);
-        molecule.addBond(1, 2, 1);
-        
+    public void permuteCompletely(Molecule molecule) {
         String signature = 
             new MoleculeSignature(molecule).getMolecularSignature();
-        int[] identity = new int[] {0, 1, 2};
+        int[] identity = new int[molecule.getAtomCount()];
+        for (int i = 0; i < molecule.getAtomCount(); i++) { identity[i] = i; }
         System.out.println(molecule + "\t" 
                 + Arrays.toString(identity) + "\t" + signature);
         
@@ -38,5 +31,51 @@ public class PermutationTest {
             Assert.assertEquals(signature, permutedSignature);
         }
     }
+    
+    @Test
+    public void permuteCNOMolecule() {
+        Molecule molecule = new Molecule();
+        molecule.addAtom("C");
+        molecule.addAtom("N");
+        molecule.addAtom("O");
+        molecule.addBond(0, 1, 1);
+        molecule.addBond(1, 2, 1);
+        permuteCompletely(molecule);
+    }
+    
+    @Test
+    public void permuteOCCCSC() {
+        Molecule molecule = new Molecule();
+        molecule.addAtom("O");
+        molecule.addAtom("C");
+        molecule.addAtom("C");
+        molecule.addAtom("C");
+        molecule.addAtom("S");
+        molecule.addAtom("C");
+        molecule.addBond(0, 1, 2);
+        molecule.addBond(1, 2, 1);
+        molecule.addBond(2, 3, 1);
+        molecule.addBond(3, 4, 1);
+        molecule.addBond(4, 5, 1);
+        permuteCompletely(molecule);
+    }
+    
+    @Test
+    public void permuteOCCOCO() {
+        Molecule molecule = new Molecule();
+        molecule.addAtom("O");
+        molecule.addAtom("C");
+        molecule.addAtom("C");
+        molecule.addAtom("O");
+        molecule.addAtom("C");
+        molecule.addAtom("O");
+        molecule.addBond(0, 1, 1);
+        molecule.addBond(1, 2, 1);
+        molecule.addBond(2, 3, 1);
+        molecule.addBond(3, 4, 1);
+        molecule.addBond(4, 5, 2);
+        permuteCompletely(molecule);
+    }
+    
 
 }
