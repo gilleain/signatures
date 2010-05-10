73b9420373b20db5ba060a83bb8908fc364c7466
maclean <gilleain.torrance@gmail.com>
2010-05-10 13:51:59 +0100

    Patch from Ola

--- src/main/java/signature/chemistry/MoleculeReader.java
+++ src/main/java/signature/chemistry/MoleculeReader.java
@@ -2,13 +2,17 @@ package signature.chemistry;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MoleculeReader {
-    
+
     public static Molecule readMolfile(String filename) {
         Molecule molecule = null;
         try {
@@ -28,11 +32,36 @@ public class MoleculeReader {
         return molecule;
     }
     
+    
+	/**
+	 * Read a list of Molecule from SDFile
+	 * @param filename path to SDFile
+	 * @return List of Molecules
+	 */
     public static List<Molecule> readSDFFile(String filename) {
+
+        File file = new File(filename);
+        FileInputStream fi;
+        try {
+            fi = new FileInputStream(file);
+            return readSDFfromStream(fi);
+        } catch (FileNotFoundException e) {
+            System.err.println(e.toString());
+        }
+        return null;
+    }
+    
+	/**
+	 * Read a list of Molecule from an InputStream, providing SDFile contents
+	 * @param stream InputStream to read from
+	 * @return List of Molecules
+	 */
+    public static List<Molecule> readSDFfromStream(InputStream stream) {
         List<Molecule> molecules = new ArrayList<Molecule>();
         try {
-            File file = new File(filename);
-            BufferedReader reader = new BufferedReader(new FileReader(file));
+            InputStreamReader ir=new InputStreamReader(stream);
+            
+            BufferedReader reader = new BufferedReader(ir);
             String line;
             int i = 0;
             List<String> block = new ArrayList<String>();
@@ -74,16 +103,18 @@ public class MoleculeReader {
             molecule.addAtom(atomIndex, symbol);
         }
         
-        if ( atomCount > 1) {
-        	// bond block starts right after the atom block
-        	int bondLineStart = atomLineEnd;
-        	int bondLineEnd   = bondLineStart + bondCount;
-        	for (int i = bondLineStart; i < bondLineEnd; i++) {
-        		String bondLine = block.get(i);
-        		int atomNumberA = Integer.parseInt(bondLine.substring(0, 3).trim());
-        		int atomNumberB = Integer.parseInt(bondLine.substring(3, 6).trim());
-        		int order = Integer.parseInt(bondLine.substring(7, 10).trim());
-        		molecule.addBond(atomNumberA - 1, atomNumberB - 1, order);
+        if (atomCount > 1) {
+            // bond block starts right after the atom block
+            int bondLineStart = atomLineEnd;
+            int bondLineEnd = bondLineStart + bondCount;
+            for (int i = bondLineStart; i < bondLineEnd; i++) {
+                String bondLine = block.get(i);
+                int atomNumberA = 
+                    Integer.parseInt(bondLine.substring(0, 3).trim());
+                int atomNumberB = 
+                    Integer.parseInt(bondLine.substring(3, 6).trim());
+                int order = Integer.parseInt(bondLine.substring(7, 10).trim());
+                molecule.addBond(atomNumberA - 1, atomNumberB - 1, order);
         	}
         }
         return molecule;
