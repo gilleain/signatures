a99ebf977ad63b723d68d4c86000dfb78ed6e2e2
maclean <gilleain.torrance@gmail.com>
2010-05-25 00:30:28 +0100

    New invariant class - should really rename these things

---
+++ src/main/java/signature/InvariantIntIntPair.java
@@ -0,0 +1,46 @@
+package signature;
+
+public class InvariantIntIntPair {
+    
+    public int label;
+    
+    public int value;
+    
+    public int originalIndex;
+    
+    public InvariantIntIntPair(int label, int value, int originalIndex) {
+        this.label = label;
+        this.value = value;
+        this.originalIndex = originalIndex;
+    }
+    
+    public boolean equals(int label, int value) {
+        return this.value == value && this.label == label;
+    }
+    
+    public boolean equals(InvariantIntIntPair o) {
+        return this.value == o.value && this.label == o.label;
+    }
+
+    public int compareTo(InvariantIntIntPair o) {
+        int c = (this.label == o.label)? 0 :
+            ((this.label < o.label)? -1 : 1);
+        if (c == 0) {
+            if (this.value < o.value) {
+                return -1;
+            } else if (this.value > o.value){
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            return c;
+        }
+    }
+
+    public String toString() {
+        return this.label + "|" + this.value + "|" + this.originalIndex;
+    }
+    
+
+}
