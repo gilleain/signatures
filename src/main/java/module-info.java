1e9dc986875cf4d64dd971fc07393e8b874e0773
gilleain <gilleain.torrance@gmail.com>
2020-10-17 12:41:33 +0100

    Add a module info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module signatures {
+	exports signature;
+	exports signature.chemistry;
+	exports signature.edgecolored;
+	
+	requires java.desktop;
+	
+	// remove this for maven!!
+	requires junit;
+}
